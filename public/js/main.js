759fb0426779aef9e500699aad57556dd04aa5af
kikiluvv <jrwenis@gmail.com>
2023-05-16 11:43:14 -0700

    media queries

--- public/js/main.js
+++ public/js/main.js
@@ -50,4 +50,13 @@ function showService(serviceName) {
   
   
   
-  
+  var docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
+    }
+  }
+);
